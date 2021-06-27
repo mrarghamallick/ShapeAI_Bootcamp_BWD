2fb5a0a1c3cf5a3ed4c18ac27f5fab049ea36fb2
Argha Mallick <86346861+mrarghamallick@users.noreply.github.com>
2021-06-27 14:01:58 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>AMARNATH YATRA</h1>
+      <p>Kolkata by air & others by tempo traveller - 8 Nights 9 Days</p>
+    </div>
+  );
+}
+
+export default Info;
